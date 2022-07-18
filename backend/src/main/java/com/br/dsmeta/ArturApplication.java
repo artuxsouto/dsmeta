146220b459de35ff4615f74dcd668b8ad63344ec
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-07-18 20:07:59 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/br/dsmeta/ArturApplication.java
@@ -0,0 +1,13 @@
+package com.br.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArturApplication.class, args);
+	}
+
+}
